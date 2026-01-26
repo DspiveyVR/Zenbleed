c6f421401c3ec4930a51c6ffd5933cc4da192136
Jacob <jacob@JAOB421L.attlocal.net>
2026-01-26 16:46:38 -0500

    Added definitions for new variables and functions.

--- source/PluginEditor.h
+++ source/PluginEditor.h
@@ -21,14 +21,21 @@ private:
     PluginProcessor& processorRef;
     std::unique_ptr<melatonin::Inspector> inspector;
     std::unique_ptr<juce::FileChooser> chooser;
-    
+
     juce::TextButton inspectButton { "Inspect the UI" };
     juce::TextButton openButton { "Open" }; /**< Opens the file explorer to allow the user to select an audio sample. */
     juce::ComboBox speedRangeBox;
+    
+    // Text buttons for knob control
+    juce::TextButton lowSpeed { "Low" };
+    juce::TextButton midSpeed { "Mid" };
+    juce::TextButton highSpeed { "High" };
+    juce::Slider speedKnob;
 
     std::unique_ptr<juce::AudioProcessorValueTreeState::ComboBoxAttachment> speedRangeAttachment;
 
     void openButtonClicked();
+    void speedKnobButtonClicked();
     PluginProcessor& getProcessor() const;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PluginEditor)
